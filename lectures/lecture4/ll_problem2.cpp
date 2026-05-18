8a3727113f14cc38685eaee5703ae1a913d9a3e8
Toby Donaldson <tjd@sfu.ca>
2026-05-18 11:14:01 -0700

    added lecture 4

---
+++ lectures/lecture4/ll_problem2.cpp
@@ -0,0 +1,46 @@
+// ll_problem2_sol.cpp
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+//
+// Starting from the solution to problem 1, do the following:
+//
+// a. When the head pointer is nullptr, the list is empty. So, write a function
+//    called is_empty() that returns true if the list is empty, false otherwise.
+// b. Write a function called print() that prints the contents of the list in
+//    order.
+// c. Write a function called size() that returns the number of nodes in the
+//    list.
+// d. Write a function called add_front(string value) that adds a new node with
+//    the given value to the front of the list.
+// e. Write add a function called remove_front() that removes the first node
+//    from the list
+// f. Write a function called clear_list() that deletes all the nodes in the
+//    list. Hint: use some of the functions you already wrote to make this
+//    easier.
+//
+
+struct Node
+{
+    string data;
+    Node* next;
+};
+
+
+int main()
+{
+    // head = nullptr;
+    // add_front("BOS");
+    // add_front("ATL");
+    // add_front("MSP");
+    // add_front("LAX");
+
+    // print();
+    // cout << "Size: " << size() << endl;
+
+    // clear_list();
+} // main
---
+++ lectures/lecture4/ll_problem3.cpp
@@ -0,0 +1,29 @@
+// ll_problem3.cpp
+
+#include <iostream>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+//
+// Starting from the solution to problem 2, rewrite the code in a class called
+// List. All the functions should be methods. The initial list should be empty,
+// and the destructor clear the list.
+//
+
+
+int main()
+{
+    // List airports;
+
+    // airports.add_front("BOS");
+    // airports.add_front("ATL");
+    // airports.add_front("MSP");
+    // airports.add_front("LAX");
+
+    // airports.print();
+    // cout << "Size: " << airports.size() << endl;
+
+    // // destructor called automatically: no memory leak
+} // main
